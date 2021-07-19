abd62bce01bc7b343a592eb3b459cfb1cd4fe906
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-07-19 01:14:01 +0000

    admin panel : display categories

--- juljula_marketplace/app/Http/Controllers/CategoryController.php
+++ juljula_marketplace/app/Http/Controllers/CategoryController.php
@@ -12,11 +12,12 @@ class CategoryController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
      */
     public function index()
     {
-        dd('display records');
+        $categories=Category::get();
+        return view('backend.category.index', compact('categories'));
     }
 
     /**
