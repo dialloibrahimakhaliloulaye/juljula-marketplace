dd032c62740c98ca08a5419dd748a6ebfc400882
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-07-28 21:08:55 +0000

    update user password

--- juljula_marketplace/resources/views/profile/index.blade.php
+++ juljula_marketplace/resources/views/profile/index.blade.php
@@ -7,38 +7,41 @@
             </div>
             <div class="col-md-5">
                 <div class="card">
-                    <div class="card-header">Metter à jour le profile</div>
+                    <div class="card-header">Modifier le profile</div>
                     <div class="card-body">
 
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                @if(session('error'))
-                    <div class="alert alert-danger">
-                        {{session('error')}}
-                    </div>
+                @if(session('status')==='password-updated')
+                    <div class="alert alert-success">Mot de passe modifié !</div>
                 @endif
-                @if(session('success'))
-                    <div class="alert alert-success">
-                        {{session('success')}}
-                    </div>
-                @endif
-                <form action="" method="post">@csrf
+                <form action="{{route('user-password.update')}}" method="post">@csrf
+                    @method('PUT')
                     <div class="card">
                         <div class="card-header">changer de mot de passe</div>
                         <div class="card-body">
                             <div class="form-group">
                                 <label for="">Mot de passe en cours</label>
                                 <input type="password" name="current_password" class="form-control">
+                                @error('current_password')
+                                    <di>{{$message}}</di>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <label for="">Nouveau mot de passe</label>
-                                <input type="password" name="new_password" class="form-control">
+                                <input type="password" name="password" class="form-control">
+                                @error('password')
+                                <di>{{$message}}</di>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <label for="">Confirmer nouveau mot de passe</label>
-                                <input type="password" name="confirm_password" class="form-control">
+                                <input type="password" name="password_confirmation" class="form-control">
+                                @error('password_confirmation')
+                                <di>{{$message}}</di>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <button class="btn btn-danger" type="submit">Mettre à jour</button>
